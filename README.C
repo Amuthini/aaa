12695a9daf7f02cd5db4e44f9921df655d0d6ac6
Amuthini <amuthini.pks05@gmail.com>
2016-07-08 11:42:35 +0530

    Create README.C
    finish

---
+++ README.C
@@ -0,0 +1,13 @@
+# aaa
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf(enter any num);
+scanf("%d",&a);
+if((a>0){printf("num is positive");}
+else if((a<0){printf("num is negative");}
+else if((a=0){printf("num is zero");}
+
+}
